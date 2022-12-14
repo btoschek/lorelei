e375581419db5275a2889e7877105275ded085b2
btoschek <bastian.toschek@protonmail.com>
2022-12-14 22:28:09 +0100

    Make play command put songs in queue & reformat

--- src/modules/music/command_play.rs
+++ src/modules/music/command_play.rs
@@ -1,15 +1,23 @@
-use serenity::client::Context;
-use serenity::builder::CreateApplicationCommand;
-use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
-use serenity::model::prelude::command::CommandOptionType;
-use serenity::model::prelude::interaction::application_command::CommandDataOptionValue;
+use serenity::{
+    builder::CreateApplicationCommand,
+    client::Context,
+    model::{
+        application::interaction::application_command::ApplicationCommandInteraction,
+        prelude::{
+            command::CommandOptionType, interaction::application_command::CommandDataOptionValue,
+        },
+    },
+};
+
+use songbird::input::restartable::Restartable;
 
 pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> String {
-
     // Ensure bot is in the voice channel
     super::command::join::run(ctx, interaction).await;
 
-    let url = interaction.data.options
+    let url = interaction
+        .data
+        .options
         .get(0)
         .expect("Expected URL string")
         .resolved
@@ -23,26 +31,29 @@ pub async fn run(ctx: &Context, interaction: &ApplicationCommandInteraction) -> 
 
     if !url.starts_with("http") {
         // log_msg_err(msg.channel_id.say(&ctx.http, "Must provide a valid URL").await);
-
         return "Error".to_string();
     }
 
     let guild_id = interaction.guild_id.unwrap();
-    let manager = songbird::get(ctx).await
-        .expect("Songbird Voice client placed in at initialisation.").clone();
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
 
     if let Some(handler_lock) = manager.get(guild_id) {
         let mut handler = handler_lock.lock().await;
 
-        let source = match songbird::ytdl(&url).await {
+        // Use lazy restartable sources to not pay
+        // for decoding of tracks which aren't actually live yet
+        let source = match Restartable::ytdl(url.clone(), true).await {
             Ok(source) => source,
             Err(_why) => {
                 // log_msg_err(msg.channel_id.say(&ctx.http, format!("{:?}", why)).await);
                 return "Error".to_string();
-            },
+            }
         };
 
-        handler.play_source(source);
+        handler.enqueue_source(source.into());
 
         //log_msg_err(msg.channel_id.say(&ctx.http, "Playing song").await);
     } else {
