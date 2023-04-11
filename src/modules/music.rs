9bebc2340447d881b6790cfaf5c65379d527d2fe
btoschek <btoschek@protonmail.com>
2023-04-11 13:07:58 +0200

    Add missing TrackRequesterId for a02210c

--- src/modules/music.rs
+++ src/modules/music.rs
@@ -1,17 +1,28 @@
-use serenity::{client::Context, model::application::command::Command};
+use serenity::{
+    client::Context,
+    model::{application::command::Command, prelude::UserId},
+    prelude::TypeMapKey,
+};
 
 pub mod command_join;
 pub mod command_leave;
 pub mod command_play;
 pub mod command_skip;
-mod status;
 mod events;
+mod status;
 
 pub mod command {
     pub use super::command_join as join;
     pub use super::command_leave as leave;
     pub use super::command_play as play;
     pub use super::command_skip as skip;
+}
+
+
+struct TrackRequesterId;
+
+impl TypeMapKey for TrackRequesterId {
+    type Value = UserId;
 }
 
 #[allow(unused_must_use)]
